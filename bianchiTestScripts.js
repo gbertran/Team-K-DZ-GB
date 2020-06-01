19f876a91a94495215f6de5f85c9e353c68bac83
Mr. Bianchi <MisterBianchi@gmail.com>
2020-05-31 23:05:42 -0400

    json edits -JAB

--- bianchiTestScripts.js
+++ bianchiTestScripts.js
@@ -9,24 +9,14 @@ function hwFunction() {
     document.getElementById("hwParagraph").innerHTML = "Hello World";
   }
 
-
-
-  //listen for a click on a certain element of the HTML
+//listen for a click on a certain element of the HTML
 document.getElementById("gimmeButton").onclick = function() {gimmeFunction()};
 
-//an action that can happen inside the HTML
+
+//Pulling data from a JSON file
 function gimmeFunction() {
 
     var numEntries = 0;
-
-    // fetch("./Bertran.json")
-    //     .then(function(resp){
-    //         return resp.json();
-    //     })
-    //     .then(function(data){
-    //         console.log(data);
-    //     });
-
 
     console.log("Fetching a JSON!");
     var filePath = "Bertran.json";
@@ -36,21 +26,19 @@ function gimmeFunction() {
         console.log(response);
         return response.json();
     }).then(data => {
+
+        //working with the data from the JSON
         console.log(data);
         document.getElementById("transcriptData").src = data;
-
-
 
         for (var i = 0; i < data.length; i++) {
             var transcriptEntry = data[i];
     
             numEntries++;
             document.getElementById("gimmeParagraph").innerHTML = transcriptEntry.Table;
-    
         }    
     
         alert(numEntries);
-
 
 
     });
